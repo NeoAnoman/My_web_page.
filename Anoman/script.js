cc9cd583f59b093500ed4a0026edcc508a73c67a
Neo Anoman <42651314+NeoAnoman@users.noreply.github.com>
2019-01-13 02:47:22 +0530

    My_web_page..
    just another html document

---
+++ Anoman/script.js
@@ -0,0 +1,17 @@
+/*var a=document.querySelectorAll('li');
+console.log(a);
+for (var i=0;i<a.length;i++)
+{
+  a[i].addEventListener('mouseover',function(){
+    this.classList.toggle('hover');});
+    console.log(a[i]);
+    a[i].addEventListener('mouseout',function(){
+      this.classList.toggle('hover');});
+      console.log(a[i]);
+}
+var b=document.querySelector('div.head');
+b.addEventListener('mouseover',function(){this.classList.toggle('hoverh');});
+b.addEventListener('mouseout',function(){this.classList.toggle('hoverh');});*/
+//c=document.querySelector('.img2');
+//c.addEventListener('mouseover',function(){this.classList.toggle('zoom');});
+//c.addEventListener('mouseout',function(){this.classList.toggle('zoom');});
